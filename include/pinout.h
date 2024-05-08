38fe182b29eeaf50f89cbcab55499a0face6a6b1
Joseph Ntaimo <jntaimo@gmail.com>
2024-05-08 14:03:41 -0400

    first commit

---
+++ include/pinout.h
@@ -0,0 +1,17 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+//relay pins
+#define IN1 2
+#define IN2 3
+#define IN3 4
+#define IN4 5
+#define IN5 6
+#define IN6 7
+#define IN7 8
+#define IN8 9
+
+#define SWITCH_PIN 12
+#define KNOB_PIN A0
+
+#endif
